8549033b81e751e375f14a909b5d01dbdb161f00
Eduardo Boçon <78614827+Eduardo-Bocon@users.noreply.github.com>
2024-06-04 20:17:19 -0300

    advances in account page

--- inventory_system/src/app/account/page.tsx
+++ inventory_system/src/app/account/page.tsx
@@ -5,6 +5,12 @@ export default function Page() {
         
         <main className="h-screen w-screen  m-0 flex items-center justify-center scroll-">
             <SideBar></SideBar>
+
+            <div className="text-5xl">
+                <p>Nome Sobrenome</p>
+                <p>nomesobrenome@gmail.com</p>
+                <button className="bg-[#EB9F27] rounded-2xl p-3 my-10 text-3xl">Change password</button>
+            </div>
             
         </main>
     );
