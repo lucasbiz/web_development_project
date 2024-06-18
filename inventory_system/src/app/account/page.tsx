524d4ae8df196dc5e7e48ca39cf753d7a67c29d1
Eduardo Boçon <78614827+Eduardo-Bocon@users.noreply.github.com>
2024-06-18 20:59:52 -0300

    user screen done

--- inventory_system/src/app/account/page.tsx
+++ inventory_system/src/app/account/page.tsx
@@ -1,16 +1,25 @@
 import SideBar from "../components/SideBar";
 
 export default function Page() {
+
+    const name = "Armando Campos";
+    const email = "a.campos@gmail.com"
+
     return(
         
         <main className="h-screen w-screen  m-0 flex items-center justify-center scroll-">
             <SideBar></SideBar>
 
-            <div className="text-5xl">
-                <p>Nome Sobrenome</p>
-                <p>nomesobrenome@gmail.com</p>
+            <div className="flex flex-col items-center text-4xl">
+                <img src="/user2.png" alt="" className="h-40 w-40" />
+
+                <p className="my-7">{name}</p>
+                <p className="my-7">{email}</p>
+
                 <button className="bg-[#EB9F27] rounded-2xl p-3 my-10 text-3xl">Change password</button>
             </div>
+
+            
             
         </main>
     );
