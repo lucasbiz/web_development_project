67979dac47f6211ba4f751d575d93af4dda96222
Eduardo Boçon <78614827+Eduardo-Bocon@users.noreply.github.com>
2024-06-18 20:20:43 -0300

    adjustes screen subsidiares

--- inventory_system/src/app/subsidiaries/page.tsx
+++ inventory_system/src/app/subsidiaries/page.tsx
@@ -3,8 +3,17 @@ import SideBar from "../components/SideBar";
 export default function Page() {
     return(
         
-        <main className="h-screen w-screen  m-0 flex items-center justify-center scroll-">
+        <main className="h-screen w-screen">
             <SideBar></SideBar>
+
+            <div className="ml-64">
+                <input type="text" className="text-black p-2 px-4 mt-16 ml-16 mb-10 rounded-2xl bg-slate-100 w-3/4 h-14 text-3xl  " />
+                
+                <div className="ml-16 w-3/4 h-20 bg-[#513F46] rounded-2xl"></div>
+            </div>
+
+            
+           
             
         </main>
     );
