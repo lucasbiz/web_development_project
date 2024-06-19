02f7742bd5663b01736c3ff1c5b9db27bff56477
Eduardo Boçon <78614827+Eduardo-Bocon@users.noreply.github.com>
2024-06-18 21:41:53 -0300

    adjustments in the structure

--- inventory_system/src/app/components/SideBar.tsx
+++ inventory_system/src/app/components/SideBar.tsx
@@ -12,26 +12,16 @@ const SideBar = () => {
         <main>
         <aside className="h-screen w-52 fixed top-0 left-0 justify-center">
             <div className="bg-[#1D1317] h-full flex flex-col items-center">
-
-                {/*logo e nome*/}
-                
-                <div className="flex flex-row justify-center items-center m-4 mb-14">
-                    <a href="/dashboard"  className="mt-5">
-                                Inventory System
-                    </a>
-                </div>
                 
 
                 {/*Elementos*/}
 
 
-                <ul className="w-3/4 flex flex-col justify-center items-center">
+                <ul className="w-3/4 flex flex-col justify-center items-center mt-11">
                     <li><SidebarItem href="/dashboard" icon="/home.png" /></li>
-                    <li><SidebarItem href="/totalItems" icon="/home.png" /></li>
                 
                     <li><SidebarItem href="/subsidiaries" icon="/location.png"/></li>
                     <li><SidebarItem href="/dashboardSubsidiary" icon="/location.png"/></li>
-                    <li><SidebarItem href="/subsidiarieItems" icon="/location.png"/></li>
 
                     <li><SidebarItem href="/account" icon="/user.png"/></li>
                 </ul>
