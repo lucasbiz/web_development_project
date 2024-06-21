1bfb2229b224053ff939196c3cc66bb2162cabfd
Eduardo-Bocon <eduardo.h.p.bocon@gmail.com>
2024-06-20 21:33:49 -0300

    pie chart in dashboard

---
+++ inventory_system/src/app/components/PieChart.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from 'react';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+interface PieChartProps {
+  data: any;
+  options: any;
+}
+
+const PieChart: React.FC<PieChartProps> = ({ data, options }) => {
+  return <Pie data={data} options={options} />;
+};
+
+export default PieChart;
--- inventory_system/src/app/dashboard/page.tsx
+++ inventory_system/src/app/dashboard/page.tsx
@@ -1,9 +1,112 @@
 import Link from "next/link";
 import ItemCard from "../components/ItemCard";
 import SideBar from "../components/SideBar";
+import PieChart from "../components/PieChart";
+
+export interface Item {
+    name: string;
+    sell_price: number;
+    buy_price: number;
+    quantity: number;
+  }
+  
+export const items: Record<string, Item> = {
+    item1: {
+      name: "Banana",
+      sell_price: 1.2,
+      buy_price: 1,
+      quantity: 30,
+    },
+    item2: {
+      name: "Abacaxi",
+      sell_price: 1.2,
+      buy_price: 1,
+      quantity: 20,
+    },
+    item3: {
+      name: "Maçã",
+      sell_price: 1.2,
+      buy_price: 1,
+      quantity: 13,
+    },
+    item4: {
+        name: "Caju",
+        sell_price: 1.2,
+        buy_price: 1,
+        quantity: 24,
+      },
+    item5: {
+        name: "Manga",
+        sell_price: 1.2,
+        buy_price: 1,
+        quantity: 35,
+      },
+    item6: {
+        name: "Pêra",
+        sell_price: 1.2,
+        buy_price: 1,
+        quantity: 48,
+    },
+    item7: {
+        name: "Maracuja",
+        sell_price: 1.2,
+        buy_price: 1,
+        quantity: 5,
+    },
+}
 
 
 export default function Page() {
+    const itemNames = Object.keys(items).map(key => items[key].name);
+
+    const itemQuantity = Object.keys(items).map(key => items[key].quantity);
+
+
+    // dados pro grafico
+    const data = {
+        labels: itemNames,
+        datasets: [
+        {
+            label: 'Quantity',
+            data: itemQuantity,
+            backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(20, 159, 64, 1)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(20, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        };
+      
+        const options = {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'top' as const,
+            },
+            title: {
+              display: true,
+              text: 'Distribution of Items',
+            },
+          },
+        };
+
+
+
     return(
         
         <main className="h-screen w-screen m-0 flex flex-col">
@@ -11,25 +114,27 @@ export default function Page() {
 
             <div className="bg-green-500 h-1/5 w-auto ml-52">
                 <p className="text-4xl text-white ml-5">filtros</p>
+                
             </div>
 
-            <div className="flex flex-row h-full w-full">
-                <div className="h-full w-1/2 flex flex-col items-center bg-blue-500 ml-52">
-                        <p>Graficos</p>
+            <div className="flex flex-row h-4/5 w-full">
+                <div className="h-full w-1/2 flex flex-col items-center ml-52 justify-center">
+                    <div className="h-2/3 w-2/3">
+                        <PieChart data={data} options={options} />
+                    </div>
+                    
                 </div>
                 <div className="bg-[#2F2428] w-1/2 h-full flex justify-center items-center">
 
                     <div className="bg-[#513F46] rounded-2xl h-4/5 w-3/4 flex flex-col items-center overflow-y-auto">
                         <h2 className="m-3 text-3xl">Itens in stock</h2>
-                        <ItemCard name="Banana" quantity={10} sellPrice={4} buyPrice={3.3}></ItemCard>
-                        <ItemCard name="Abacaxi" quantity={34} sellPrice={4.4} buyPrice={3.2}></ItemCard>
-                        <ItemCard name="Abacaxi" quantity={34} sellPrice={4.4} buyPrice={3.2}></ItemCard>
-                        <ItemCard name="Abacaxi" quantity={34} sellPrice={4.4} buyPrice={3.2}></ItemCard>
-                        <ItemCard name="Abacaxi" quantity={34} sellPrice={4.4} buyPrice={3.2}></ItemCard>
-                        <ItemCard name="Abacaxi" quantity={34} sellPrice={4.4} buyPrice={3.2}></ItemCard>
-                        <ItemCard name="Abacaxi" quantity={34} sellPrice={4.4} buyPrice={3.2}></ItemCard>
-                        <ItemCard name="Abacaxi" quantity={34} sellPrice={4.4} buyPrice={3.2}></ItemCard>
-                        <ItemCard name="Abacaxi" quantity={34} sellPrice={4.4} buyPrice={3.2}></ItemCard>
+                        {Object.keys(items).map((key) => {
+                            const item = items[key];
+                            return (
+                                <ItemCard name={item.name} quantity={item.quantity} sellPrice={item.sell_price} buyPrice={item.buy_price}></ItemCard>
+                            );
+                        })}
+                        
                     </div>
                         
                 </div>
