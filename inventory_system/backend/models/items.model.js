52fe487c5c3671d136af43729ccda29d01113df8
lucasbiz <lucaasbizteixeira@gmail.com>
2024-06-30 13:04:06 -0300

    adicionando comentarios ao codigo

--- inventory_system/backend/models/items.model.js
+++ inventory_system/backend/models/items.model.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
-const AutoIncrement = require ('mongoose-sequence')(mongoose)
 
+// Importa o plugin mongoose-sequence para auto-incrementar campos
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+// Define o schema do item, que representa a estrutura dos documentos na coleção 'items'
 const itemSchema = new mongoose.Schema({
+    // Campo 'cod' que será um número e auto-incrementado
     cod: {
         type: Number
     },
@@ -22,6 +26,9 @@ const itemSchema = new mongoose.Schema({
     timestamps: true
 });
 
+// Aplica o plugin de auto-incremento ao schema, incrementando o campo 'cod'
 itemSchema.plugin(AutoIncrement, { inc_field: 'cod' });
 
+// Exporta o modelo 'Item' baseado no itemSchema para ser usado em outras partes da aplicação
 module.exports = mongoose.model('Item', itemSchema);
+
